d9b67d43e191faf83cca010c1849d58b689b3efc
Csaba Marosi <marosicsaba91@gmail.com>
2023-12-14 12:06:32 +0100

    Navigation Removed

--- Scripts/EditorConstants.cs
+++ Scripts/EditorConstants.cs
@@ -8,6 +8,5 @@ namespace VoxelSystem
 		public const int soOrder_VoxelShape = 1;
 		public const int soOrder_Palette = 2;
 		public const int soOrder_OctoBlock = 3;
-		public const int soOrder_Navigation = 4;
 	}
 }
